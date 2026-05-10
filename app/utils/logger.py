2225b9776c608b94d724e89e40b9ad8e9d72e427
Marat Sultanov <maratsultanov2@gmail.com>
2026-05-10 22:24:27 +0300

    Delete app/utils/logger.py

--- app/utils/logger.py
+++
@@ -1,7 +0,0 @@
-import logging
-
-log = logging.getLogger("tat7_logistics")
-log.setLevel(logging.INFO)
-handler = logging.StreamHandler()
-handler.setFormatter(logging.Formatter("%(asctime)s %(levelname)s %(message)s"))
-log.addHandler(handler)
